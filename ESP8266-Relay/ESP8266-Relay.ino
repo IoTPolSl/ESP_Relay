553525dde10540237eec9de336679f4cfae5569d
Yoga720\Sheerley Cerebral <echelon.div@gmail.com>
2018-07-22 12:49:16 +0200

    Tested code, updated schematic

--- ESP8266-Relay/ESP8266-Relay.ino
+++ ESP8266-Relay/ESP8266-Relay.ino
@@ -7,20 +7,21 @@
 #define RELAY_OFF 0
 
 #define Relay_1   2
+#define Relay_2   3
 
 //------------------------------------------------------------------------
 //                          variables
 //------------------------------------------------------------------------
 
-const char* ssid = "New_IP"; //your WiFi Name
-const char* password = "Password1+";  //Your Wifi Password
+const char* ssid = "esp"; //your WiFi Name
+const char* password = "12345678";  //Your Wifi Password
 
 WiFiServer server(80);
 
 int sleepTime = 60000000; // 5min
 
 IPAddress WAP = (192, 168, 0, 1);
-IPAddress RPi_1 = (192, 168, 0, 122);
+IPAddress RPi_1 = (192, 168, 0, 107);
 
 //------------------------------------------------------------------------
 //                          setup
@@ -30,6 +31,7 @@ void setup()
   Serial.begin(9600);
   Serial.setTimeout(2000);
   while(!Serial) { }
+  Serial.println();
   Serial.println("I'm awake.");
   
   Serial.print("Connecting to ");
@@ -47,7 +49,7 @@ void setup()
   
   server.begin();
   Serial.println("Server started");
-  Serial.print(WiFi.localIP());
+  Serial.println(WiFi.localIP());
 }
 
 //------------------------------------------------------------------------
@@ -55,17 +57,28 @@ void setup()
 //------------------------------------------------------------------------
 void loop() 
 {
+  //my_setup();
+  
   if (my_ping(WAP) == false)
   {
+    Serial.println("reseting WAP");
+    digitalWrite(Relay_1, RELAY_OFF);
+    delay(5000);
+    digitalWrite(Relay_1, RELAY_ON);
+  }
+
+  if (my_ping(RPi_1) == false)
+  {
+    Serial.println("reseting RPi_1");
     digitalWrite(Relay_1, RELAY_OFF);
     delay(5000);
     digitalWrite(Relay_1, RELAY_ON);
   }
 
   Serial.print("Going into deep sleep for ");
-  Serial.print(sleepTime);
-  Serial.println(" seconds.");
-  ESP.deepSleep(sleepTime);
+  Serial.print(sleepTime);                       
+  Serial.println(" microseconds.");
+  ESP.deepSleep(sleepTime); 
 }
 
 //------------------------------------------------------------------------
@@ -78,7 +91,12 @@ bool my_ping(IPAddress ip)
   for (int i = 0; i <= 4; i++)
   {
     ret = Ping.ping(ip);
-    if (ret == true) return true;
+    if (ret == true) 
+    {
+      Serial.println("OK");
+      return true;
+    }
+    Serial.println("Target not reached.");
     delay(500);
   }
   
@@ -90,6 +108,7 @@ void my_setup()
   Serial.begin(9600);
   Serial.setTimeout(2000);
   while(!Serial) { }
+  Serial.println();
   Serial.println("I'm awake.");
   
   Serial.print("Connecting to ");
@@ -107,6 +126,6 @@ void my_setup()
   
   server.begin();
   Serial.println("Server started");
-  Serial.print(WiFi.localIP());
+  Serial.println(WiFi.localIP());
 }
 
